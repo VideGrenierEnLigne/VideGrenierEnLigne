9f1636aa1840aafcfd1c89c675704f5adb61a638
Titouan Massé <titouanmasselaude@gmail.com>
2025-05-17 14:41:26 +0200

    :tada: added sh scripts to start and stop the prod env :tada:

---
+++ docker/prod-env/stop-prod.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Aller dans le dossier du script, peu importe où il est lancé
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+if [ -d "prod-fetch" ]; then
+    echo "prod-fetch folder found. Stopping Docker containers..."
+
+    docker compose -f ./docker-compose.yml down
+    if [ $? -eq 0 ]; then
+        echo "Docker containers stopped successfully."
+    else
+        echo "Error: Failed to stop Docker containers."
+        exit 1
+    fi
+
+    echo "Removing prod-fetch folder..."
+    rm -rf prod-fetch
+    if [ $? -eq 0 ]; then
+        echo "prod-fetch folder removed successfully."
+    else
+        echo "Error: Failed to remove prod-fetch folder."
+        exit 1
+    fi
+
+    echo "Cleanup done."
+else
+    echo "prod-fetch folder not found. Production does not seem to be running."
+fi
